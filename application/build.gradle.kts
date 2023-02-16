048f900a2e60f99437cb1c9c90460034c2c2f6e5
Orinda Harrison <harrison@regulusweb.com>
2023-02-16 17:52:41 +0300

    DRY dependency declaration

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -2,11 +2,6 @@ dependencies {
     implementation(project(":features:orders"))
     implementation(project(":features:employees"))
 
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-hateoas")
-
     runtimeOnly("com.h2database:h2")
 }
 
--- features/employees/build.gradle.kts
+++ features/employees/build.gradle.kts
@@ -1,10 +1,5 @@
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
 
     kapt("org.springframework.boot:spring-boot-configuration-processor")
-
-    implementation("org.springframework.boot:spring-boot-starter")
 }
