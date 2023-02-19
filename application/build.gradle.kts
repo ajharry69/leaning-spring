d2b3cdfe87bfe70545088f35f1deea69195e920b
Orinda Harrison <harrison@regulusweb.com>
2023-02-19 11:36:27 +0300

    Serving web contents

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,7 +1,9 @@
 dependencies {
     implementation(project(":features:orders"))
     implementation(project(":features:employees"))
+    implementation(project(":features:greetings"))
 
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.ldap:spring-ldap-core")
     implementation("org.springframework.security:spring-security-ldap")
